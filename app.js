721e2653d6841ca18194a23a231ac7022d22137a
Juan Manuel Rodríguez <patexowin@hotmail.es>
2015-07-19 19:35:48 +0200

    Validacion de entradas

--- app.js
+++ app.js
@@ -41,9 +41,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('error',{
             message: err.message,
-            error: err
+            error: err,
+            errors: []
         });
     });
 }
@@ -52,9 +53,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error',{
         message: err.message,
-        error: {}
+        error: {},
+        errors: []
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 /* GET author. */
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Autor' });
+  res.render('author', { title: 'Autor', errors: []});
 });
 
 // Autoload de comandos con ;quizId
