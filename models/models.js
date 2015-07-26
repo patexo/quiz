27c0b03b5ffdfaee7c3a31eb42fd1c75ebd31077
Juan Manuel Rodríguez <patexowin@hotmail.es>
2015-07-26 10:33:22 +0200

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -27,8 +27,18 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 //Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path); //antes:var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// Importar definición de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
